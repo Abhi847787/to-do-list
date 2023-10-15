d57a1a232a1d18c7ef30f5f24f2d6b078d5d8a36
Abhishek Kumar Gautam <akg.20u10321@btech.nitdgp.ac.in>
2023-10-15 15:01:42 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+//use express.json() to get data into json format
+app.use(express.json());
+//Port 
+const PORT = 5500;
+
+//use cors
+app.use(cors());
+
+//import routes
+const TodoItemRoute = require('./routes/todoItems');
+
+
+//connect to mongodb ..
+mongoose.connect("mongodb://127.0.0.1:27017/to-do-list")
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+
+
+app.use('/', TodoItemRoute);
+
+
+
+//connect to server
+app.listen(PORT, ()=> console.log("Server connected") );
