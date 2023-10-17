874cc85ff9cacb7585b1ed9e19c5a5c7994fb795
Abhi847787 <107705619+Abhi847787@users.noreply.github.com>
2023-10-17 15:14:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ const TodoItemRoute = require('./routes/todoItems');
 
 
 //connect to mongodb ..
-mongoose.connect("mongodb://127.0.0.1:27017/to-do-list")
+mongoose.connect(
+    "mongodb+srv://Abhishek_111:abhishek@to-do-list1.4yykivr.mongodb.net/?retryWrites=true&w=majority"
+  )
 .then(()=> console.log("Database connected"))
 .catch(err => console.log(err))
 
